d89b06b658252b57a92fc83a512e81de38d4af2a
Jason Rundell <jason.rundell@gmail.com>
2014-05-27 00:20:55 -0400

    1.0.0 release!
    Gruntfile refined down to closer to what I wanted
    Package refined (removed: hashres and htmlmin)
    SRC folders revamped to include a couple different versions of
    frameworks
    index.jade has a better test of Angular working or not

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 /*!
- * FireShell Gruntfile
- * http://getfireshell.com
- * @author Todd Motto
+ * Sassy Swine Gruntfile
+ * http://sassyswine.com
+ * @author Jason Rundell
  */
 
 'use strict';
@@ -11,252 +11,299 @@
  */
 var LIVERELOAD_PORT = 35729;
 var lrSnippet = require('connect-livereload')({
-  port: LIVERELOAD_PORT
+    port: LIVERELOAD_PORT
 });
 var mountFolder = function (connect, dir) {
-  return connect.static(require('path').resolve(dir));
+    return connect.static(require('path').resolve(dir));
 };
 
 module.exports = function(grunt) {
 
-  // Load grunt tasks
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    // Load grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  // Project configuration.
-  grunt.initConfig({
+    // Project configuration.
+    grunt.initConfig({
 
-    /**
-     * Connect port/livereload
-     * https://github.com/gruntjs/grunt-contrib-connect
-     * Starts a local webserver and injects
-     * livereload snippet
-     */
-    connect: {
-      options: {
-        port: 9000,
-        hostname: '*'
-      },
-      livereload: {
-        options: {
-          middleware: function (connect) {
-            return [lrSnippet, mountFolder(connect, 'app')];
-          }
-        }
-      }
-    },
-
-    /**
-     * Jade
-     * https://github.com/gruntjs/grunt-contrib-jade
-     * Node template engine
-     */
-    jade: {
-      compile: {
-        files: {
-          '<%= project.app %>/index.html': ['<%= project.src %>/jade/index.jade']
+        /**
+         * CONFIG
+         * Set project info
+         */
+        project: {
+            src:     'src',
+            srcJS:   'src/js',
+            srcSCSS: 'src/scss',
+            srcJADE: 'src/jade',
+            app:     'app',
+            appCSS:  'app/css',
+            appJS:   'app/js',
+            appHTML: 'app'
         },
-        options: {
-          pretty: true
-        }
-      }
-    },
 
-    /**
-     * JSHint
-     * https://github.com/gruntjs/grunt-contrib-jshint
-     * Manage the options inside .jshintrc file
-     */
-    jshint: {
-      files: {
-        all: ['Gruntfile.js', 'src/js/*.js']
-      },
-      options: {
-        "node": true,
-        "browser": true,
-        "es5": true,
-        "esnext": true,
-        "bitwise": true,
-        "camelcase": true,
-        "curly": true,
-        "eqeqeq": true,
-        "immed": true,
-        "indent": 2,
-        "latedef": true,
-        "newcap": true,
-        "noarg": true,
-        "quotmark": "single",
-        "regexp": true,
-        "undef": true,
-        "unused": true,
-        "strict": true,
-        "trailing": false,
-        "smarttabs": true,
-        "reporter": require('jshint-stylish'),
-        "globals" : {
-          "jQuery": true,
-          "Modernizr": true
-        }
-      }
-    },
+        /**
+         * CONFIG
+         * Package details (used for 'tag')
+         */
+        pkg: grunt.file.readJSON('package.json'),
 
-    /**
-     * Opens the web server in the browser
-     * https://github.com/jsoverson/grunt-open
-     */
-    open: {
-      server: {
-        path: 'http://localhost:<%= connect.options.port %>'
-      }
-    },
-
-    pkg: grunt.file.readJSON('package.json'),
-
-    /**
-     * Set project info
-     */
-    project: {
-      src: 'src',
-      app: 'app',
-      css: [
-        '<%= project.src %>/scss/style.scss'
-      ],
-      js: [
-        '<%= project.src %>/js/*.js'
-      ]
-    },
-
-    /**
-     * Compile Sass/SCSS files
-     * https://github.com/gruntjs/grunt-contrib-sass
-     * Compiles all SCSS files and appends project banner
-     */
-    sass: {
-      dev: {
-        options: {
-          style: 'expanded',
-          banner: '<%= tag.banner %>'
+        /**
+         * CONFIG
+         * Project banner
+         * Dynamically appended to CSS/JS files
+         * Inherits text from package.json
+         */
+        tag: {
+            banner: '/*!\n' +
+            ' * <%= pkg.name %>\n' +
+            ' * <%= pkg.description %>\n' +
+            ' * <%= pkg.homepage %>\n' +
+            ' * @author <%= pkg.author %>\n' +
+            ' * @version <%= pkg.version %>\n' +
+            ' * @repository <%= pkg.repository.url %>\n' +
+            ' * @license <%= pkg.license %>\n' +
+            ' */\n'
         },
-        files: {
-          '<%= project.app %>/css/style.css': '<%= project.css %>'
-        }
-      },
-      dist: {
-        options: {
-          style: 'compressed'
+
+        /**
+         * LAUNCH
+         * Connect port/livereload
+         * https://github.com/gruntjs/grunt-contrib-connect
+         * Starts a local webserver and injects
+         * livereload snippet
+         */
+        connect: {
+            options: {
+                port: 9000,
+                hostname: '*'
+            },
+            livereload: {
+                options: {
+                    middleware: function (connect) {
+                        return [lrSnippet, mountFolder(connect, 'app')];
+                    }
+                }
+            }
         },
-        files: {
-          '<%= project.app %>/css/style.min.css': '<%= project.css %>'
+
+        /**
+         * LAUNCH
+         * Opens the web server in the browser
+         * https://github.com/jsoverson/grunt-open
+         */
+        open: {
+            server: {
+                path: 'http://localhost:<%= connect.options.port %>'
+            }
+        },
+
+        /**
+         * LAUNCH + WATCH
+         * JSHint
+         * https://github.com/gruntjs/grunt-contrib-jshint
+         * Manage the options inside .jshintrc file
+         */
+        jshint: {
+            options: {
+                node: true,
+                browser: true,
+                esnext: true,
+                bitwise: true,
+                camelcase: true,
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                indent: 4,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                quotmark: "single",
+                regexp: true,
+                undef: true,
+                unused: true,
+                strict: true,
+                trailing: false,
+                smarttabs: true,
+                reporter: require('jshint-stylish'),
+                "globals" : {
+                    // jQuery: true,
+                    // foundation: true,
+                    // zepto: true,
+                    angular: true
+                }
+            },
+            files: {
+                src: ['src/js/2-app.js']
+            }
+        },
+
+        /**
+         * LAUNCH + WATCH
+         * Jade
+         * https://github.com/gruntjs/grunt-contrib-jade
+         * Node template engine
+         */
+        jade: {
+            debug: {
+                files: {
+                    '<%= project.appHTML %>/index.html': ['<%= project.srcJADE %>/index.jade']
+                },
+                options: {
+                    pretty: true
+                }
+            },
+            release: {
+                files: {
+                    '<%= project.appHTML %>/index.html': ['<%= project.srcJADE %>/index.jade']
+                },
+                options: {
+                    pretty: false
+                }
+            }
+        },
+
+        /**
+         * LAUNCH + WATCH
+         * Compile Sass/SCSS files
+         * https://github.com/gruntjs/grunt-contrib-sass
+         * Compiles all SCSS files and appends project banner
+         */
+        sass: {
+            dev: {
+                options: {
+                    banner: '<%= tag.banner %>'
+                },
+                files: {
+                    '<%= project.appCSS %>/style.css': '<%= project.srcSCSS %>/style.scss'
+                }
+            },
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    '<%= project.appCSS %>/style.css': '<%= project.srcSCSS %>/style.scss'
+                }
+            }
+        },
+
+        /**
+         * LAUNCH + WATCH
+         * Uglify
+         * https://github.com/gruntjs/grunt-contrib-uglify
+         */
+        uglify: {
+            my_target: {
+                options: {
+                    banner: '<%= tag.banner %>'
+                },
+                files: {
+                    '<%= project.appJS %>/scripts.js': ['<%= project.srcJS %>/*.js']
+                }
+            }
+        },
+
+
+        /**
+         * WATCH
+         * Runs tasks against changed watched files
+         * https://github.com/gruntjs/grunt-contrib-watch
+         * Watching development files and run concat/compile tasks
+         * Livereload the browser once complete
+         */
+        watch: {
+            configFiles: {
+                files: [
+                    'Gruntfile.js'
+                ],
+                options: {
+                    event: [
+                        'changed'
+                    ],
+                    reload: true
+                },
+                tasks: [
+                    'open'
+                ]
+            },
+            scripts: {
+                files: [
+                    '<%= project.srcJS %>/*.js',
+                    '<%= project.srcSCSS %>/**/*.scss',
+                    '<%= project.srcJADE %>/**/*.jade',
+                    'package.json'
+                ],
+                tasks: [
+                    'jshint',
+                    'uglify',
+                    'jade:debug',
+                    'sass:dev'
+                ],
+                options: {
+                    event: [
+                        'changed',
+                        'added',
+                        'deleted'
+                    ],
+                    reload: true
+                }
+            },
+            livereload: {
+                options: {
+                    livereload: LIVERELOAD_PORT,
+                    ignore: [] // add something to ignore in here, if you desire
+                },
+                files: [
+                    '<%= project.app %>/**/*.html',
+                    '<%= project.app %>/**/*.css',
+                    '<%= project.app %>/**/*.js',
+                    '<%= project.app %>/**/*.png',
+                    '<%= project.app %>/**/*.jpg',
+                    '<%= project.app %>/**/*.gif',
+                    '<%= project.app %>/**/*.svg'
+                ]
+            }
         }
-      }
-    },
+
+    });
 
     /**
-     * Project banner
-     * Dynamically appended to CSS/JS files
-     * Inherits text from package.json
+     * TASK
+     * Default task
+     * Run `grunt` on the command line to run locally
+     * Builds local development quality assets and opens browser
      */
-    tag: {
-      banner: '/*!\n' +
-              ' * <%= pkg.name %>\n' +
-              ' * <%= pkg.description %>\n' +
-              ' * <%= pkg.homepage %>\n' +
-              ' * @author <%= pkg.author %>\n' +
-              ' * @version <%= pkg.version %>\n' +
-              ' * @repository <%= pkg.repository.url %>\n' +
-              ' * @license <%= pkg.license %>\n' +
-              ' */\n'
-    },
+    grunt.registerTask('default', [
+        'jshint',
+        'uglify',
+        'jade:debug',
+        'sass:dev',
+        'connect:livereload',
+        'open',
+        'watch'
+    ]);
 
     /**
-     * Uglify
-     * https://github.com/gruntjs/grunt-contrib-uglify
+     * TASK
+     * Stage push task
+     * Run `grunt build` on the command line
+     * Builds production quality assets
      */
-    uglify: {
-      my_target: {
-        options: {
-          banner: '<%= tag.banner %>'
-        },
-        files: {
-          'app/js/scripts.min.js': ['src/js/*.js']
-        }
-      }
-    },
-
+    grunt.registerTask('build', [
+        'uglify',
+        'sass:dist',
+        'jade:release'
+    ]);
 
     /**
-     * Runs tasks against changed watched files
-     * https://github.com/gruntjs/grunt-contrib-watch
-     * Watching development files and run concat/compile tasks
-     * Livereload the browser once complete
+     * TASK
+     * Production push task
+     * Run `grunt push` on the command line
+     * TO DO: Eventually, this command will be what you use to push assets to a CDN, server, or X.
      */
-    watch: {
-      jshint: {
-        files: '<%= project.src %>/js/*.js',
-        tasks: ['jshint']
-      },
-      uglify: {
-        files: '<%= project.src %>/js/*.js',
-        tasks: ['uglify']
-      },
-      jade: {
-        files: '<%= project.src %>/jade/**/*.jade',
-        tasks: ['jade']
-      },
-      sass: {
-        files: '<%= project.src %>/scss/**/*.scss',
-        tasks: ['sass:dev']
-      },
-      livereload: {
-        options: {
-          livereload: LIVERELOAD_PORT,
-          ignore: [] // add something to ignore in here, if you desire
-        },
-        files: [ // all of the filez!
-          '<%= project.app %>/**/*.html',
-          '<%= project.app %>/**/*.css',
-          '<%= project.app %>/**/*.js',
-          '<%= project.app %>/**/*.png',
-          '<%= project.app %>/**/*.jpg',
-          '<%= project.app %>/**/*.gif',
-          '<%= project.app %>/**/*.svg'
-        ]
-      }
-    }
-
-
-  });
-
-  // Default task(s).
-  /**
-   * Default task
-   * Run `grunt` on the command line to run locally
-   */
-  grunt.registerTask('default', [
-    'sass:dev',
-    'jade',
-    'uglify',
-    'jshint',
-    'connect:livereload',
-    'open',
-    'watch'
-  ]);
-
-  /**
-   * Stage push task
-   * Run `grunt build` on the command line
-   */
-  grunt.registerTask('build', [
-    'sass:stage'
-  ]);
-
-  /**
-   * Production push task
-   * Run `grunt push` on the command line
-   */
-  grunt.registerTask('push', [
-    'sass:dist',
-    'jshint',
-    'uglify'
-  ]);
+    grunt.registerTask('push', [
+        'uglify',
+        'sass:dist',
+        'jade:release'
+    ]);
 
 };
